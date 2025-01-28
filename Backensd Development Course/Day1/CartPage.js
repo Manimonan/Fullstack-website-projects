55bb8567e4111a034a5fd220e6e42d724a93c04b
Manimonan <167786934+Manimonan@users.noreply.github.com>
2025-01-28 09:50:00 +0000

    Default & named exports

---
+++ Backensd Development Course/Day1/CartPage.js
@@ -0,0 +1,14 @@
+let addtoCart=()=>{
+    return 'Cart quantity is 5'
+}
+// this is a default export
+//module.exports= addtoCart;
+
+let cartProd =()=>{
+    return "Book of Web Development"
+}
+let cartTotal =()=>{
+    return "350"
+}
+// this is named export
+module.exports={cartProd,cartTotal}
--- Backensd Development Course/Day1/index.js
+++ Backensd Development Course/Day1/index.js
@@ -1,2 +1,9 @@
-console.log("Hallow Manimohan")
-console.log(20+30)
+//const addtoCart = require("./CartPage")
+const {cartProd,cartTotal} = require("./CartPage")
+
+
+console.log("User Name")
+console.log(90+30)
+console.log('hallow')
+console.log(cartTotal())
+console.log(cartProd())
